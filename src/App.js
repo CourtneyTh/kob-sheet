730d6a358244831660ad9f59dc4112fd87ca61c4
courtney <courtney.code@outlook.com>
2024-12-30 15:23:18 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Sheet from "./Components/Sheet/Sheet";
+import "./App.css";
+
+function App() {
+  return(
+    <div className="app">
+      <Sheet />
+    </div>
+  )
+  }
+
+export default App;
---
+++ src/Components/Sheet/Sheet.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Sheet.css';
+
+function Sheet(){
+    return(
+        <div className="sheet">
+            <h3>Name:</h3>
+            <h3>Age:</h3>
+            <h3>Fear:</h3>
+            <h3>Motivation:</h3>
+            <h3>Flaws:</h3>
+            <h3>Description:</h3>
+        </div>
+    );
+}
+
+export default Sheet;
