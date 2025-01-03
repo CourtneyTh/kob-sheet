23150487a2dbcb896487ad871390776507634c12
courtney <courtney.code@outlook.com>
2025-01-03 12:33:17 +0000

    add character info editor component to sheet

--- src/Components/Sheet/Sheet.js
+++ src/Components/Sheet/Sheet.js
@@ -1,15 +1,11 @@
 import React from "react";
 import './Sheet.css';
+import CharacterInfoEditor from "../CharacterInfo/CharacterInfoEditor.js";
 
 function Sheet(){
     return(
         <div className="sheet">
-            <h3>Name:</h3>
-            <h3>Age:</h3>
-            <h3>Fear:</h3>
-            <h3>Motivation:</h3>
-            <h3>Flaws:</h3>
-            <h3>Description:</h3>
+            <CharacterInfoEditor />
         </div>
     );
 }
